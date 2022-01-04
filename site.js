b7efa29668ae901e0a42386999a86e5134a464e0
geforcesong <georgeguo@outlook.com>
2022-01-04 11:08:10 +0800

    updated

---
+++ site.js
@@ -0,0 +1,25 @@
+document.querySelector("#btnPopup").addEventListener("click", () => {
+  GS.Dialog.getInstance().show(
+    `<p class="f3">Popup Title</p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
+        eaque neque assumenda, nesciunt nulla quidem, quae deserunt dolor
+        illo ipsa corrupti molestiae corporis nisi placeat ipsam sequi magni
+        necessitatibus distinctio!
+      </p>`,
+    { autoClose: true }
+  );
+});
+
+document.querySelector("#btnPopup1").addEventListener("click", () => {
+    GS.Dialog.getInstance().show(
+      `<p class="f3">Popup Title</p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
+          eaque neque assumenda, nesciunt nulla quidem, quae deserunt dolor
+          illo ipsa corrupti molestiae corporis nisi placeat ipsam sequi magni
+          necessitatibus distinctio!
+        </p>`
+    );
+  });
+  
