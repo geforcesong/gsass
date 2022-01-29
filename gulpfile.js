62b57018ea2fea87906d4389d243b35649bc8e3d
geforcesong <georgeguo@outlook.com>
2022-01-29 09:48:23 +0800

    added purge

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 const gulp = require("gulp");
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-// const purgeCss = require("gulp-purgecss");
+const purgeCss = require("gulp-purgecss");
 
 const scssFiles = "**/*.scss";
 const htmlFiles = "**/*.html";
+const jsFiles = "**/*.js";
 
 function buildStyles() {
   return (
     src(scssFiles)
       .pipe(sass())
-      // .pipe(purgeCss({ content: [htmlFiles] }))
+      .pipe(purgeCss({ content: [htmlFiles, jsFiles] }))
       .pipe(dest("dist"))
   );
 }
